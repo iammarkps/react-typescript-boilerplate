fbfa699669edcf7e24b0af30ab9e8b837fec1ef0
iammarkps <markkung08@gmail.com>
2019-04-11 15:13:22 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+export const App = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>React Typescript Boilerplate</title>
+    </Helmet>
+    <div>
+      <h1>React Typescript Boilerplate</h1>
+    </div>
+  </React.Fragment>
+)
